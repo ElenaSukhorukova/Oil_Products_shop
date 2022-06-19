1f1fb20cbbe4abf73a072faf757471da4e542659
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-06-19 16:22:50 +0300

    Add parameters for a password in the model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  include ActiveModel::SecurePassword
+
   has_secure_password
+  has_secure_password :password, validations: false
+  attr_accessor :password_digest, :recovery_password_digest
 end
