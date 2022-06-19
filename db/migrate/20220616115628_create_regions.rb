45f7e58b1cc6c90eed04dd26f8defd3f7482c12d
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-06-19 11:30:47 +0300

    add columns in model Region. Fill table Regions data.

--- db/migrate/20220616115628_create_regions.rb
+++ db/migrate/20220616115628_create_regions.rb
@@ -1,6 +1,8 @@
 class CreateRegions < ActiveRecord::Migration[7.0]
   def change
     create_table :regions do |t|
+      t.text :name_region
+      t.string :code_region
 
       t.timestamps
     end
