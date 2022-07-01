e075444e74a7322cbe41cae31a346c0aca15f859
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-01 10:29:22 +0300

    Add a new controller 'user'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/new'
   get 'home/index'
   root 'home#index'
+
+  # resources
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
